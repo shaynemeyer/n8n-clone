2e208a19104b63119075fcf8920e8e32377f668f
Shayne Meyer <shayne.meyer@gmail.com>
2025-11-08 08:13:12 -0800

    added trpc and established fetching pattern

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import { Button } from '@/components/ui/button';
+import { getQueryClient, trpc } from '@/trpc/server';
+import { Client } from './Client';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
 
-export default function Home() {
+export default async function Home() {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <Button>Click me</Button>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 }
